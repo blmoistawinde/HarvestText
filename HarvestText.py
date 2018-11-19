fbcb084e281b8364cd518e0368e1b218e9cd8805
blmoistawinde <blmoistawinde@github.com>
2018-11-19 11:39:27 +0800

    Initial commit

---
+++ HarvestText.py
@@ -0,0 +1,229 @@
+#coding=utf-8
+import os
+import numpy as np
+import pandas as pd
+import jieba
+import jieba.posseg as pseg
+from collections import defaultdict
+from word_discoverer import WordDiscoverer
+from sent_dict import SentDict
+
+
+class HarvestText:
+    def __init__(self,standard_name=False):
+        self.standard_name = standard_name             # 是否使用连接到的实体名来替换原文
+        self.entity_types = set()
+        self.trie_root = {}
+
+    def build_trie(self, new_word, entity, entity_type):
+        type0 = "#%s#" % entity_type
+        if not type0 in self.entity_types:
+            self.entity_types.add(type0)
+        trie_node = self.trie_root
+        for ch in new_word:
+            if not ch in trie_node:
+                trie_node[ch] = {}
+            trie_node = trie_node[ch]
+        if not 'leaf' in trie_node:
+            trie_node['leaf'] = {(entity,type0)}
+        else:
+            trie_node['leaf'].add((entity,type0))
+    def add_entities(self, entity_mention_dict, entity_type_dict):
+        self.entity_mention_dict = entity_mention_dict
+        self.entity_type_dict = entity_type_dict
+        type_entity_mention_dict = defaultdict(dict)
+        for entity0,type0 in entity_type_dict.items():
+            type_entity_mention_dict[type0][entity0] = entity_mention_dict[entity0]
+        self.type_entity_mention_dict = type_entity_mention_dict
+        self._add_entities(type_entity_mention_dict)
+        
+    def _add_entities(self,type_entity_mention_dict):
+        for type0 in type_entity_mention_dict:
+            entity_mention_dict0 = type_entity_mention_dict[type0]
+            for entity0 in entity_mention_dict0:
+                mentions = entity_mention_dict0[entity0]
+                for mention0 in mentions:
+                    self.build_trie(mention0,entity0,type0)
+        self.prepare()
+    def add_new_words(self,new_words):
+        for word in new_words:
+            self.build_trie(word,word,"新词")
+        self.prepare()
+    def prepare(self):
+        for type0 in self.entity_types:
+            jieba.add_word(type0, freq=10000, tag=type0[1:-1])
+    def dig_trie(self, sent, l):  # 返回实体右边界r,实体范围
+        trie_node = self.trie_root
+        for i in range(l, len(sent)):
+            if sent[i] in trie_node:
+                trie_node = trie_node[sent[i]]
+            else:
+                if "leaf" in trie_node:
+                    return i, trie_node["leaf"]
+                else:
+                    return -1, set()  # -1表示未找到
+        # 收尾
+        if "leaf" in trie_node:
+            return len(sent), trie_node["leaf"]
+        else:
+            return -1, set()  # -1表示未找到
+    def choose_from(self,name0, entities):
+        # TODO: 加入更多同名不同实体的消歧策略
+        return list(entities)[0]
+    def entity_linking(self,sent):
+        entities_info = []
+        l = 0
+        while l < len(sent):
+            r, entities = self.dig_trie(sent, l)
+            if r != -1:
+                name0 = sent[l:r]
+                entities_info.append(([l, r], self.choose_from(name0, entities)))  # 字典树能根据键找到实体范围，选择则依然需要根据历史等优化
+                l = r
+            else:
+                l += 1
+        return entities_info
+    def decoref(self, sent, entities_info):
+        left = 0
+        processed_text = ""
+        for (beg, end), (entity,e_type) in entities_info:
+            # processed_text += sent[left:beg] + entity
+            processed_text += sent[left:beg] + e_type
+            left = end
+        processed_text += sent[left:]
+        return processed_text
+    def posseg(self, sent, standard_name = False):
+        self.standard_name = standard_name
+        entities_info = self.entity_linking(sent)
+        sent2 = self.decoref(sent, entities_info)
+        result = []
+        i = 0
+        for word, flag in pseg.cut(sent2):
+            if word in self.entity_types:
+                if self.standard_name:
+                    word = entities_info[i][1][0]     # 使用链接的实体
+                else:
+                    l,r = entities_info[i][0]        # 或使用原文
+                    word = sent[l:r]
+                i +=1
+            result.append((word, flag))
+        return result
+    def seg(self, sent, return_sent=False, standard_name = False):
+        self.standard_name = standard_name
+        entities_info = self.entity_linking(sent)
+        sent2 = self.decoref(sent, entities_info)
+        result = []
+        i = 0
+        for word in jieba.cut(sent2):
+            if word in self.entity_types:
+                if self.standard_name:
+                    word = entities_info[i][1][0]     # 使用链接的实体
+                else:
+                    l,r = entities_info[i][0]        # 或使用原文
+                    word = sent[l:r]
+                i +=1
+            result.append(word)
+        if return_sent:
+            return " ".join(result)
+        else:
+            return result
+    def word_discover(self, doc, threshold_seeds=[], auto_param = True,
+                      excluding_types = [], excluding_words=[],     # 可以排除已经登录的某些种类的实体，或者某些指定词
+                      max_word_len=5, min_freq=0.00005, min_entropy=1.4, min_aggregation=50,
+                      ent_threshold="both",mem_saving=0):
+        # 采用经验参数，此时后面的参数设置都无效
+        if auto_param:               # 根据自己的几个实验确定的参数估计值，没什么科学性，但是应该能得到还行的结果
+            length = len(doc)
+            min_entropy = np.log(length) / 8
+            min_freq = min(0.00005,20.0/length)
+            min_aggregation = np.sqrt(length) / 10
+            mem_saving = int(length>300000)
+            # ent_threshold: 确定左右熵的阈值对双侧都要求"both"，或者只要左右平均值达到"avg"
+            # 对于每句话都很极短的情况（如长度<8），经常出现在左右边界的词语可能难以被确定，这时ent_threshold建议设为"avg"
+        ws = WordDiscoverer(doc,max_word_len,min_freq,min_aggregation, min_entropy, ent_threshold ,mem_saving)
+        info = {"text":[],"freq":[],"left_ent":[],"right_ent":[],"agg":[]}
+        if len(excluding_types) > 0:
+            ex_mentions = [x for enty in self.entity_mention_dict
+                             if self.entity_type_dict[enty] in excluding_types
+                             for x in entity_mention_dict[enty]]
+        else:
+            ex_mentions = []
+        ex_mentions += excluding_words
+        for w in ws.word_infos:
+            if w.text in ex_mentions:
+                continue
+            info["text"].append(w.text)
+            info["freq"].append(w.freq)
+            info["left_ent"].append(w.left)
+            info["right_ent"].append(w.right)
+            info["agg"].append(w.aggregation)
+        info = pd.DataFrame(info)
+        info = info.set_index("text")
+        # 词语质量评分
+        info["score"] = np.log10(info["agg"])*info["freq"]*(info["left_ent"]+info["right_ent"])
+        # 利用种子词来确定筛选优质新词的标准，种子词中最低质量的词语将被保留（如果一开始就被找到的话）
+        if len(threshold_seeds) > 0:
+            min_score = 100000
+            for seed in threshold_seeds:
+                if seed in info.index:
+                    min_score = min(min_score,info.loc[seed,"score"])
+            if (min_score >= 100000):
+                min_score = 0
+            else:
+                min_score *= 0.9          # 留一些宽松的区间
+                info = info[info["score"] > min_score]
+        return info
+    def build_sent_dict(self, sents, method="PMI",min_times=5, pos_seeds=[],neg_seeds=[]):
+        docs = [self.seg(sent) for sent in sents]
+        self.sent_dict = SentDict(docs, method, min_times, pos_seeds, neg_seeds)
+        return self.sent_dict
+    def analyse_sent(self, sent):
+        words = [word for word in self.seg(sent) if word in self.sent_dict.words]
+        if len(words) > 0:
+            return sum(self.sent_dict[word] for word in words) / len(words)
+        else:
+            return 0.0
+if __name__ == "__main__":
+    ht = HarvestText()
+    
+    para = "上港的武磊和恒大的郜林，谁是中国最好的前锋？武磊吧，他是射手榜第一，原来是弱点的单刀也有了进步"
+    
+    
+    # these 2 seeds can be obtained from some structured knowledge base
+    print("add entity info(mention, type)")
+    entity_mention_dict = {'武磊':['武磊','武球王'],'郜林':['郜林','郜飞机'],'前锋':['前锋'],'上海上港':['上港'],'广州恒大':['恒大'],'单刀球':['单刀']}
+    entity_type_dict = {'武磊':'球员','郜林':'球员','前锋':'位置','上海上港':'球队','广州恒大':'球队','单刀球':'术语'}
+    print(entity_mention_dict)
+    print(entity_type_dict)
+    ht.add_entities(entity_mention_dict,entity_type_dict)
+    print("\nSentence segmentation")
+    print(ht.seg(para,return_sent=True))
+    print("\nPOS tagging with entity types")
+    for word, flag in ht.posseg(para):
+        print("%s:%s" % (word, flag),end = " ")
+    print("\n\nentity_linking")
+    for span, entity in ht.entity_linking(para):
+        print(span, entity)
+
+    #new_words_info = ht.word_discover(para,excluding_types=["球员"])  # 防止新词覆盖已知的球员名
+    #new_words_info = ht.word_discover(para, threshold_seeds=["武磊"])
+    new_words_info = ht.word_discover(para)
+    print("\nnew words detection")
+    print(new_words_info)
+    new_words = new_words_info.index.tolist()
+    # 添加识别到的新词，在后续的分词中将会优先分出这个词，词性为"新词"
+    #ht.add_new_words(new_words)
+    
+    # 情感词典的构建及情感分析
+    print("\nsentiment dictionary")
+    sents = ["武磊威武，中超第一射手！",
+          "武磊强，中超最第一本土球员！",
+          "郜林不行，只会抱怨的球员注定上限了",
+          "郜林看来不行，已经到上限了"]
+    sent_dict = ht.build_sent_dict(sents,min_times=1,pos_seeds=["第一"],neg_seeds=["不行"])
+    print("%s:%f" % ("威武",sent_dict["威武"]))
+    print("%s:%f" % ("球员",sent_dict["球员"]))
+    print("%s:%f" % ("上限",sent_dict["上限"]))
+    print("\nsentence sentiment")
+    sent = "武球王威武，中超最强球员！"
+    print("%f:%s" % (ht.analyse_sent(sent),sent))
+    
---
+++ word_discoverer.py
@@ -0,0 +1,216 @@
+#coding=utf-8
+
+import re
+import time
+import math
+from collections import defaultdict
+
+def genSubstr(string, n):
+    """
+    Generate all substrings of max length n for string
+    """
+    length = len(string)
+    res = [string[i: j] for i in range(0, length)
+        for j in range(i + 1, min(i + n + 1, length + 1))]
+    return res
+
+def genSubparts(string):
+    """
+    Partition a string into all possible two parts, e.g.
+    given "abcd", generate [("a", "bcd"), ("ab", "cd"), ("abc", "d")]
+    For string of length 1, return empty list
+    """
+    length = len(string)
+    res = [(string[0:i], string[i:]) for i in range(1, length)]
+    return res
+
+def entropyOfList(cnt_dict):
+    length = sum(v for v in cnt_dict.values())
+    return sum([-v/length*math.log(v/length) for v in cnt_dict.values()])
+
+def indexOfSortedSuffix(doc, max_word_len):
+    """
+    Treat a suffix as an index where the suffix begins.
+    Then sort these indexes by the suffixes.
+    """
+    indexes = []
+    length = len(doc)
+    indexes = [(i,j) for i in range(0, length)
+               for j in range(i + 1, min(i + 1 + max_word_len, length + 1))]
+    return sorted(indexes, key=lambda i_j: doc[i_j[0]:i_j[1]])
+
+
+class WordInfo(object):
+    """
+    Store information of each word, including its freqency, left neighbors and right neighbors
+    """
+    def __init__(self, text):
+        super(WordInfo, self).__init__()
+        self.text = text
+        self.freq = 0.0
+        self.left = defaultdict(int)
+        self.right = defaultdict(int)
+        self.aggregation = 0
+
+    def update(self, left, right):
+        """
+        Increase frequency of this word, then append left/right neighbors
+        @param left a single character on the left side of this word
+        @param right as left is, but on the right side
+        """
+        self.freq += 1
+        if left: self.left[left] += 1
+        if right: self.right[right] += 1
+
+    def compute(self, length):
+        """
+        Compute frequency and entropy of this word
+        @param length length of the document for training to get words
+        """
+        self.freq /= length
+        self.left = entropyOfList(self.left)
+        self.right = entropyOfList(self.right)
+        
+    def computeAggregation(self, words_dict):
+        """
+        Compute aggregation of this word
+        @param words_dict frequency dict of all candidate words
+        """
+        parts = genSubparts(self.text)
+        if len(parts) > 0:
+            self.aggregation = min([self.freq/words_dict[p1_p2[0]].freq/words_dict[p1_p2[1]].freq for p1_p2 in parts])
+
+
+
+class WordDiscoverer(object):
+    def __init__(self, doc, max_word_len=5, min_freq=0.00005, min_entropy=2.0, min_aggregation=50,
+                 ent_threshold="both",mem_saving=0):
+        super(WordDiscoverer, self).__init__()
+        self.max_word_len = max_word_len
+        self.min_freq = min_freq
+        self.min_entropy = min_entropy
+        self.min_aggregation = min_aggregation
+        
+        if mem_saving == 0:
+            self.word_infos = self.genWords(doc)
+            # Filter out the results satisfy all the requirements
+            if ent_threshold == "both":
+                filter_func = lambda v: len(v.text) > 1 and v.aggregation > self.min_aggregation and\
+                            v.freq > self.min_freq and v.left > self.min_entropy and v.right > self.min_entropy
+            else:
+                filter_func = lambda v: len(v.text) > 1 and v.aggregation > self.min_aggregation and\
+                            v.freq > self.min_freq and (v.left+v.right)/2.0 > self.min_entropy
+        else:
+            # 对于太长的语料，因为每个候选词都保存了列表形式的left和right，导致内存容易爆炸，考虑优化算法。
+            # 先根据简单的规则筛选词语（freq,agg）再重新扫描统计left,right
+            # 因为需要两次扫描语料，所以速度会稍微慢一点
+            self.word_infos = self.genWords2(doc)
+            if ent_threshold == "both":
+                filter_func = lambda v: v.left > self.min_entropy and v.right > self.min_entropy
+            else:
+                filter_func = lambda v: (v.left+v.right)/2.0 > self.min_entropy
+        self.word_infos = list(filter(filter_func, self.word_infos))
+        self.word_with_freq = [(w.text, w.freq) for w in self.word_infos]
+        self.words = [w[0] for w in self.word_with_freq]
+        # Result infomations, i.e., average data of all words
+        word_count = float(len(self.word_with_freq))
+        self.avg_len = sum([len(w.text) for w in self.word_infos])/word_count
+        self.avg_freq = sum([w.freq for w in self.word_infos])/word_count
+        self.avg_left_entropy = sum([w.left for w in self.word_infos])/word_count
+        self.avg_right_entropy = sum([w.right for w in self.word_infos])/word_count
+        self.avg_aggregation = sum([w.aggregation for w in self.word_infos])/word_count
+
+    def genWords(self, doc):
+        """
+        Generate all candidate words with their frequency/entropy/aggregation informations
+        @param doc the document used for words generation
+        """
+        #pattern = re.compile('[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“”‘’｛｝【】（）…￥！—┄－]+')
+        # numbers preserved
+        pattern = re.compile('[\\s,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“”‘’｛｝【】（）…￥！—┄－]+')
+        doc = re.sub(pattern, ' ', doc)
+        suffix_indexes = indexOfSortedSuffix(doc, self.max_word_len)
+        word_cands = {}
+        # compute frequency and neighbors
+        for suf in suffix_indexes:
+            word = doc[suf[0]:suf[1]]
+            if " " in word : continue
+            if word not in word_cands:
+                word_cands[word] = WordInfo(word)
+            word_cands[word].update(doc[suf[0] - 1:suf[0]], doc[suf[1]:suf[1] + 1])
+            
+        # compute probability and entropy
+        length = len(doc)
+        self.length = length
+        for k in word_cands:
+            word_cands[k].compute(length)
+        # compute aggregation of words whose length > 1
+        values = sorted(list(word_cands.values()), key=lambda x: len(x.text))
+        for v in values:
+            if len(v.text) == 1: continue
+            v.computeAggregation(word_cands)
+        return sorted(values, key=lambda v: v.freq, reverse=True)
+
+    def genWords2(self, doc):
+        """
+        Generate all candidate words with their frequency/entropy/aggregation informations
+        @param doc the document used for words generation
+        """
+        #pattern = re.compile('[\\s\\d,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“”‘’｛｝【】（）…￥！—┄－]+')
+        # numbers preserved
+        pattern = re.compile('[\\s,.<>/?:;\'\"[\\]{}()\\|~!@#$%^&*\\-_=+a-zA-Z，。《》、？：；“”‘’｛｝【】（）…￥！—┄－]+')
+        doc = re.sub(pattern, ' ', doc)
+        suffix_indexes = indexOfSortedSuffix(doc, self.max_word_len)
+        word_cands = {}
+        # compute frequency and neighbors
+        for suf in suffix_indexes:
+            word = doc[suf[0]:suf[1]]
+            if " " in word : continue
+            if word not in word_cands:
+                word_cands[word] = WordInfo(word)
+            word_cands[word].freq += 1
+            
+        self.length = len(doc)
+        for k in word_cands:
+            word_cands[k].freq /= self.length
+        values = word_cands.values()
+        for v in values:
+            if len(v.text) == 1: continue
+            v.computeAggregation(word_cands)
+        filter_func = lambda v: len(v.text) > 1 and v.aggregation > self.min_aggregation and\
+                    v.freq > self.min_freq
+        values = list(filter(filter_func, values))
+        word_cands = {w:word_cands[w] for w in word_cands if word_cands[w] in values}
+        for suf in suffix_indexes:
+            word = doc[suf[0]:suf[1]]
+            if word in word_cands:
+                left, right = doc[suf[0] - 1:suf[0]], doc[suf[1]:suf[1] + 1]
+                if left: word_cands[word].left[left] += 1
+                if right: word_cands[word].right[right] += 1
+        
+        values = word_cands.values()
+        for v in values:
+            v.left = entropyOfList(v.left)
+            v.right = entropyOfList(v.right)
+        return values
+
+
+if __name__ == '__main__':
+   doc = '十四是十四四十是四十，，十四不是四十，，，，四十不是十四'
+   ws = WordDiscoverer(doc, max_word_len=2, min_aggregation=1.2, min_entropy=0.4)
+   N = ws.length
+   
+   print("new words with freqency:")
+   print(' '.join(['%s:%f'% (w,f) for (w,f) in ws.word_with_freq]))
+   print("new words info:")
+   print('\n'.join(['[%s] times:%d, left:%f, right:%f, aggregation:%f'%(w.text,w.freq*N,w.left,w.right,w.aggregation) for w in  ws.word_infos]))
+   print('average len: ', ws.avg_len)
+   print('average frequency: ', ws.avg_freq)
+   print('average left entropy: ', ws.avg_left_entropy)
+   print('average right entropy: ', ws.avg_right_entropy)
+   print('average aggregation: ', ws.avg_aggregation)
+
+        
+    
+
+
