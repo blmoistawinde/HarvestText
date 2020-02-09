f780529986fbdb2ab9b6e5c49657fd6a8c18b2a1
Zhiling Zhang <1840962220@qq.com>
2020-02-09 21:11:20 +0800

    Updating version number to automatically publish to pypi

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, blmoistawinde'
 author = 'blmoistawinde'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.2'
+release = '0.7.2.dev0'
 
 github_doc_root = 'https://github.com/blmoistawinde/HarvestText/tree/master/doc'
 
